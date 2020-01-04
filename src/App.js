4d71fbbfad4f85b147fcb7aabeaafcaa4de03864
phlinhng <phlinhng@outlook.com>
2020-01-05 00:52:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import pinyin from 'pinyin';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    nowTexting: null,
+    nowPinyin: Array(0)
+  };
+
+  hwaMap = (chr) => {
+    if(chr[0].match(/^x/)){
+      return [chr[0].replace('x','s')];
+    }else{
+      return chr;
+    }
+  }
+
+  hwaConvert = (nowText) => {
+    let hanyu = pinyin(nowText, {style: pinyin.STYLE_NORMAL});
+    hanyu.forEach( (chr, index, thisArray) => {
+      thisArray[index] = this.hwaMap(chr);
+    });
+    return hanyu;
+  }
+  
+  handleText = event => {
+    const nowTexting = event.target.value;
+    const nowPinyin = this.hwaConvert(nowTexting);
+    this.setState({ nowTexting, nowPinyin });
+  }
+
+  render() {
+    const { nowTexting, nowPinyin } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          華語姓名羅馬化轉換器
+        </header>
+        <div className="App-Panel">
+          <TextField style={{ marginBottom: "8px" }} value={ nowTexting } onChange={this.handleText}></TextField>
+          <span>{ nowPinyin.join(" ") }</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
