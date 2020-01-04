eb2066030f4b691ae705c4bcb36deacd7c201e2f
phlinhng <phlinhng@outlook.com>
2020-01-05 04:14:55 +0800

    Updates

---
+++ precache-manifest.4a4714bacca09c3a8b98ce86996bc802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2cc3a2079c1bfe8c27d7dd14100220",
+    "url": "/hwa-convertor/index.html"
+  },
+  {
+    "revision": "19a869dc91e5fc71d428",
+    "url": "/hwa-convertor/static/css/main.98207d9a.chunk.css"
+  },
+  {
+    "revision": "c4c13770f6c282e7c528",
+    "url": "/hwa-convertor/static/js/2.9219ba6f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/hwa-convertor/static/js/2.9219ba6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19a869dc91e5fc71d428",
+    "url": "/hwa-convertor/static/js/main.1088da16.chunk.js"
+  },
+  {
+    "revision": "cbc7afead591cac45bb3",
+    "url": "/hwa-convertor/static/js/runtime-main.99eae49c.js"
+  }
+]);
