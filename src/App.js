0e17a267128b986746c9943e21ab1650e8d99e0d
phlinhng <phlinhng@outlook.com>
2020-01-05 04:10:28 +0800

    first realse

--- src/App.js
+++ src/App.js
@@ -10,10 +10,39 @@ class App extends Component {
   };
 
   hwaMap = (chr) => {
-    if(chr[0].match(/^x/)){
-      return [chr[0].replace('x','s')];
+    let newPinyin = chr[0];
+
+    if(newPinyin.match(/^[nljqx]/)){
+      newPinyin = newPinyin.replace('q','c').replace('x','s');
+      if(!newPinyin.match(/iu$/)){
+        newPinyin = newPinyin.replace('u','yu');
+      }
+    }else if(newPinyin.match(/^c[aeou]/)){
+      newPinyin = newPinyin.replace('c','ts');
+    }
+
+    if(newPinyin.match(/^[zcs]hi$/)){
+      return [newPinyin.replace('i','j')];
+    }else if(newPinyin.match(/^ri$/)){
+      return [newPinyin.replace('i','hj')];
+    }else if(newPinyin.match(/^zi$/)){
+      return [newPinyin.replace('i','j')];
+    }else if(chr[0].match(/^[cs]i$/)){
+      return [newPinyin.replace('i','ze')];
+    }else if(newPinyin.match(/^[dtnlgkhzs]e$/) || newPinyin.match(/^[zcst].e$/)){
+      return [newPinyin.replace('e','er')];
+    }else if(newPinyin.match(/ei$/)){
+      return [newPinyin.replace('ei','ey')];
+    }else if(newPinyin.match(/iu$/)){
+      return [newPinyin.replace('iu','iou')];
+    }else if(newPinyin === 'you'){
+      return [newPinyin.replace('ou','oo')];
+    }else if(newPinyin.match(/ian$/) || newPinyin.match(/^yan$/)){
+      return [newPinyin.replace('an','en')];
+    }else if(newPinyin.match(/un$/)){
+      return [newPinyin.replace('un','eun').replace('yeun','eun')];
     }else{
-      return chr;
+      return [newPinyin];
     }
   }
 
@@ -36,12 +65,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          華語姓名羅馬化轉換器
+          華語羅馬化轉換器
         </header>
         <div className="App-Panel">
-          <TextField style={{ marginBottom: "8px" }} value={ nowTexting } onChange={this.handleText}></TextField>
-          <span>{ nowPinyin.join(" ") }</span>
+          <div style={{display: "inline-block", verticalAlign:"top" ,marginRight: "16px", width: "45vw"}}>
+            <TextField multiline maxRows="20" style={{ width: "100%" }} value={ nowTexting } onChange={this.handleText}></TextField>
+          </div>
+          <div className="Pinyin">
+            { nowPinyin.join(" ") }
+          </div>
         </div>
+        <footer className="App-footer">
+          Designed by phlinhng
+        </footer>
       </div>
     );
   }
